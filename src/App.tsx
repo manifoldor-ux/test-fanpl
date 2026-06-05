2ef16fb5b7918ea2dd89db4b9319575780c54d24
manifoldor-ux <manifoldor@gmail.com>
2026-06-05 13:23:04 +0800

    feat: add src/App.tsx with routing

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
